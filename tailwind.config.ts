5d8ca4ac6bd38a9263aa1ec0b08653a93e0189e9
khadi <Kadirid9@gmail.com>
2025-05-23 04:18:22 +0100

    font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,15 +9,38 @@ export default {
         secondary: "var(--color-secondary)",
         background: "var(--color-background)",
         text: "var(--color-text)",
+        mygreen: "var(--color-mygreen)",
+        myblack2: "var(--color-myblack2)",
+        myblack: "var(--color-myblack)",
+        myyellow: "var(--color-myyellow)",
+        mywhite: "var(--color-mywhite)",
+        darkgreen: "var(--color-darkgreen)",
+        darkgreentwo: "var(--color-darkgreentwo)",
+        mygrey: "var(--color-mygrey)",
+        mygrey2: "var(--color-mygrey2)",
       },
       fontFamily: {
-        sans: ["Inter", "sans-serif"],
-
+        inter: ["var(--font-inter)", "sans-serif"],
+     
+      },
+      fontWeight: {
+        thin: '100',
+        extralight: '200',
+        light: '300',
+        '325': '325',
+        '350': '350',
+        normal: '400',
+        book: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+        black: '900',
       },
       screens: {
-        sm: "772px", // Custom sm breakpoint at 772px
-        md: "1040px", // Custom md breakpoint at 1040px
-        lg: "1350px", // Custom lg breakpoint (Set this to your desired value)
+        sm: "772px",
+        md: "1040px",
+        lg: "1350px",
       },
     },
   },
